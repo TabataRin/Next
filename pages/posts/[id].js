27b209086220b76361d68c7f482713bf1c7bf408
TabataRin <rin_tabata@nuco.co.jp>
2024-03-07 16:01:28 +0900

    end introduction

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Layout from '../../components/layout';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
     // Add the "await" keyword like this:
@@ -10,7 +13,8 @@ export async function getStaticProps({ params }) {
         postData,
       },
     };
-  }
+}
+
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
@@ -18,16 +22,21 @@ export async function getStaticPaths() {
     fallback: false,
   };
 }
+
+
 export default function Post({ postData }) {
     return (
       <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
       </Layout>
     );
 }
